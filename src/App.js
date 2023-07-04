a592d00be686f48bc175243eef7e3dee1f6155dd
germanpagano <sergiogermanpagano@gmail.com>
2023-07-04 19:51:19 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import HeaderCom from "./components/header/HeaderCom";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import ProfilesCards from "./components/profiles/ProfilesCards";
 import { ProfilesCardsContextProvider } from "../src/context/ProfilesCardsContext";
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from "body-scroll-lock";
 
 function App() {
+  useEffect(() => {
+    const targetElement = document.querySelector("#body");
 
+    // Bloquear el desplazamiento del cuerpo
+    disableBodyScroll(targetElement);
 
-
+    // Desbloquear el desplazamiento del cuerpo cuando se desmonte el componente
+    return () => {
+      enableBodyScroll(targetElement);
+      clearAllBodyScrollLocks();
+    };
+  }, []);
   return (
     <div className="App">
       <ProfilesCardsContextProvider>
         <Router>
           <HeaderCom />
-
-         <Routes>
+          <Routes>
             <Route path="/perfil" element={"tu perfil"} />
             <Route path="/chats" element={"tus chats"} />
-            <Route path="/" element={<ProfilesCards/>}/>
-          </Routes> 
+            <Route path="/" element={<ProfilesCards />} />
+          </Routes>
         </Router>
       </ProfilesCardsContextProvider>
     </div>
